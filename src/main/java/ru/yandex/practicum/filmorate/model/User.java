3804ceaf6740cdcf1db37f4042c2fb7714636ee7
Veber <pavelbo92@yandex.ru>
2023-06-05 22:01:07 +0300

    fix: Сделано по ТЗ 9 Спринт

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.filmorate.model;
 import jakarta.validation.constraints.*;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NonNull;
 
 import java.time.LocalDateTime;
 
