811a8413443d0c244f0e8839ae52311ccea00f2b
Veber <pavelbo92@yandex.ru>
2023-06-25 20:42:20 +0300

    fix: Сделано по комментариям ревьювера ТЗ 10

--- src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.filmorate.storage;
 
 import ru.yandex.practicum.filmorate.model.User;
 import java.util.Collection;
-import java.util.Optional;
 
 public interface UserStorage {
     void addUser(User user);
